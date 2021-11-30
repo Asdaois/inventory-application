5741f202c2bec2830717c4b79f9ba221acf01f7c
Asdaois <josexx1234@gmail.com>
2021-11-30 17:57:12 -0400

    feat(models): define rental and user

---
+++ models/Rental.js
@@ -0,0 +1,29 @@
+import mongoose, { model, models, Schema } from "mongoose";
+
+const RentalSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    machinery_rented: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: Machinery,
+        required: true,
+      },
+    ],
+    init_date: {
+      type: Date,
+      required: true,
+    },
+    due_date: {
+      type: Date,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Rental || model("Rental", RentalSchema);
---
+++ models/User.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema, model } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+    },
+    company: {
+      type: String,
+    },
+    phone_number: {
+      type: Number,
+      required: true,
+    },
+    direction: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || model("User", UserSchema);
